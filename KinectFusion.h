8345c87953abc05dc53af8a07167fa38a057bb68
Stefan <stefan.gerl@tum.de>
2020-09-20 17:56:06 +1000

    PoseEstimator draft

--- KinectFusion.h
+++ KinectFusion.h
@@ -1,12 +1,22 @@
 #pragma once
 
 #include <string>
+#include <assert.h>
 #include "Eigen.h"
 #include "VirtualSensor.h"
+#include "NearestNeighbor.h"
 
 #include <opencv4/opencv2/core.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+
+struct PointCloud
+{
+    std::vector<Vector3f> points;
+    std::vector<bool> pointsValid;
+    std::vector<Vector3f> normals;
+    std::vector<bool> normalsValid;
+};
 
 // compute surface and normal maps
 class SurfaceMeasurer
@@ -33,6 +43,7 @@ public:
     void process()
     {
         computeVertexAndNormalMap();
+        std::cout << "size" << m_vertexMap.size() << std::endl;
     }
 
     void printDepthMap()
@@ -59,15 +70,20 @@ public:
         cv::waitKey(0);
     }
 
+    PointCloud getPointCloud()
+    {
+        return PointCloud{m_vertexMap, m_vertexValidityMap, m_normalMap, m_normalValidityMap};
+    }
+
 
 private:
     void computeVertexAndNormalMap()
     {
         m_vertexMap.reserve(m_DepthImageHeight*m_DepthImageWidth);
-        m_normalMap.reserve(m_DepthImageHeight*m_DepthImageWidth);
-
         m_vertexValidityMap.reserve(m_DepthImageHeight*m_DepthImageWidth);
-        m_normalValidityMap.reserve(m_DepthImageHeight*m_DepthImageWidth);
+
+        m_normalMap = std::vector<Vector3f>(m_DepthImageHeight*m_DepthImageWidth);
+        m_normalValidityMap = std::vector<bool>(m_DepthImageHeight*m_DepthImageWidth);
 
         //#pragma omp parallel for
 
@@ -83,14 +99,14 @@ private:
                 const float depth = m_rawDepthMap[idx];
                 if (depth == MINF || depth == NAN)
                 {
-                    m_vertexMap[idx] = Vector3f(MINF, MINF, MINF);
-                    m_vertexValidityMap[idx] = false;
+                    m_vertexMap.push_back(Vector3f(MINF, MINF, MINF));
+                    m_vertexValidityMap.push_back(false);
                 }
                 else
                 {
                     // backproject to camera space
-                    m_vertexMap[idx] = Vector3f((x - cX) / fovX * depth, (y - cY) / fovY * depth, depth);
-                    m_vertexValidityMap[idx] = true;
+                    m_vertexMap.push_back(Vector3f((x - cX) / fovX * depth, (y - cY) / fovY * depth, depth));
+                    m_vertexValidityMap.push_back(true);
                 }
 
             }
@@ -125,13 +141,6 @@ private:
             m_normalMap[y * m_DepthImageWidth] = Vector3f(MINF, MINF, MINF);
             m_normalMap[(m_DepthImageWidth - 1) + y * m_DepthImageWidth] = Vector3f(MINF, MINF, MINF);
         }
-
-//        for(int i= 0; i<10; ++i)
-//        {
-//            std::cout << "v " << m_vertexMap[i] << std::endl;
-//            std::cout << "n " << m_normalMap[i] << std::endl;
-
-//        }
     }
 
 
@@ -148,8 +157,109 @@ private:
 
 class PoseEstimator
 {
+public:
+    PoseEstimator()
+    {}
 
+    void setTarget(PointCloud& input)
+    {
+        m_target = input;
+    }
+    void setSource(PointCloud& input)
+    {
+        m_source = input;
+    }
+    void setTarget(std::vector<Vector3f> points, std::vector<Vector3f> normals)
+    {
+        m_target.points = points;
+        m_target.normals = normals;
+
+        m_target.normalsValid = std::vector<bool>(normals.size(), true);
+        m_target.pointsValid = std::vector<bool>(points.size(), true);
+
+    }
+    void setSource(std::vector<Vector3f> points, std::vector<Vector3f> normals)
+    {
+        m_source.points = points;
+        m_source.normals = normals;
+
+        m_source.normalsValid = std::vector<bool>(normals.size(), true);
+        m_source.pointsValid = std::vector<bool>(points.size(), true);
+    }
+
+
+    void printPoints()
+    {
+        std::cout << "first 10 points " << std::endl;
+        for(int i =0; i<std::min<int>(10, m_target.points.size());++i)
+        {
+            std::cout << m_target.points[i].transpose() << std::endl;
+        }
+    }
+    virtual Matrix4f estimatePose() = 0;
+
+    static std::vector<Vector3f> pruneVector(std::vector<Vector3f>& input, std::vector<bool>& validity)
+    {
+        assert((input.size() == validity.size()));
+
+        std::vector<Vector3f> output;
+        for (int i = 0; i < input.size(); ++i)
+        {
+            if(validity[i])
+            {
+                output.push_back(input[i]);
+            }
+        }
+        return output;
+    }
+    static std::vector<Vector3f> transformPoint(const std::vector<Vector3f>& input, const Matrix4f& pose)
+    {
+        std::vector<Vector3f> output;
+        output.reserve(input.size());
+
+        const auto rotation = pose.block(0, 0, 3, 3);
+        const auto translation = pose.block(0, 3, 3, 1);
+
+        for (const auto& point : input) {
+            output.push_back(rotation * point + translation);
+        }
+
+        return output;
+    }
+    static std::vector<Vector3f> transformNormal(const std::vector<Vector3f>& input, const Matrix4f& pose)
+    {
+        std::vector<Vector3f> output;
+        output.reserve(input.size());
+
+        const auto rotation = pose.block(0, 0, 3, 3);
+
+        for (const auto& normal : input) {
+            output.push_back(rotation.inverse().transpose() * normal);
+        }
+
+        return output;
+    }
+protected:
+    // missing_ normals
+    PointCloud m_target;
+    PointCloud m_source;
+    int m_nIter;
 };
+
+class NearestNeighborPoseEstimator : public PoseEstimator
+{
+public:
+    NearestNeighborPoseEstimator()
+        : m_nearestNeighborSearch{ std::make_unique<NearestNeighborSearchFlann>()}
+    {}
+
+    virtual Matrix4f estimatePose() override {}
+
+private:
+    std::unique_ptr<NearestNeighborSearch> m_nearestNeighborSearch;
+};
+
+
 
 class SurfaceReconstructor
 {
@@ -177,6 +287,20 @@ public:
         //m_SurfaceMeasurer->smoothInput();
         //m_SurfaceMeasurer->displayDepthMap();
         m_SurfaceMeasurer->process();
+
+        PointCloud Frame0 = m_SurfaceMeasurer->getPointCloud();
+
+        std::vector<bool> pointsAndNormalsValid;
+        pointsAndNormalsValid.reserve( Frame0.pointsValid.size() );
+
+        std::transform(Frame0.pointsValid.begin(), Frame0.pointsValid.end(), Frame0.normalsValid.begin(),
+            std::back_inserter(pointsAndNormalsValid), std::logical_and<>());
+
+
+        m_PoseEstimator = new NearestNeighborPoseEstimator();
+        m_PoseEstimator->setTarget(PoseEstimator::pruneVector(Frame0.points, pointsAndNormalsValid),
+                                   PoseEstimator::pruneVector(Frame0.normals, pointsAndNormalsValid));
+        m_PoseEstimator->printPoints();
     }
 
 
@@ -195,7 +319,7 @@ public:
 
 private:
     SurfaceMeasurer* m_SurfaceMeasurer;
-    PoseEstimator m_PoseEstimator;
+    PoseEstimator* m_PoseEstimator;
     SurfaceReconstructor m_SurfaceReconstructor;
     SurfacePredictor m_SurfacePredictor;
 
