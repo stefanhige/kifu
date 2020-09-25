feceae2a9d31772443f3e6d1ffc45c934e50333e
Stefan <stefan.gerl@tum.de>
2020-09-25 12:16:57 +1000

    PoseEstimator

--- KinectFusion.h
+++ KinectFusion.h
@@ -196,7 +196,7 @@ public:
             std::cout << m_target.points[i].transpose() << std::endl;
         }
     }
-    virtual Matrix4f estimatePose() = 0;
+    virtual Matrix4f estimatePose(Matrix4f) = 0;
 
     static std::vector<Vector3f> pruneVector(std::vector<Vector3f>& input, std::vector<bool>& validity)
     {
@@ -240,7 +240,7 @@ public:
         return output;
     }
 protected:
-    // missing_ normals
+    // missing normals
     PointCloud m_target;
     PointCloud m_source;
     int m_nIter;
@@ -253,9 +253,99 @@ public:
         : m_nearestNeighborSearch{ std::make_unique<NearestNeighborSearchFlann>()}
     {}
 
-    virtual Matrix4f estimatePose() override {}
+    virtual Matrix4f estimatePose(Matrix4f initialPose = Matrix4f::Identity()) override
+    {
+        // Build the index of the FLANN tree (for fast nearest neighbor lookup).
+        m_nearestNeighborSearch->buildIndex(m_target.points);
+
+        // The initial estimate can be given as an argument.
+        Matrix4f estimatedPose = initialPose;
+
+        for (int i = 0; i < m_nIter; ++i) {
+            auto transformedPoints = transformPoint(m_source.points, estimatedPose);
+            auto transformedNormals = transformNormal(m_source.normals, estimatedPose);
+            auto matches = m_nearestNeighborSearch->queryMatches(transformedPoints);
+
+            // TODO
+            // pruneCorrespondences(transformedNormals, m_target.getNormals(), matches);
+
+            std::vector<Vector3f> sourcePoints;
+            std::vector<Vector3f> targetPoints;
+
+            // Add all matches to the sourcePoints and targetPoints vectors,
+            // so thath sourcePoints[i] matches targetPoints[i].
+            for (int j = 0; j < transformedPoints.size(); j++)
+            {
+                const auto& match = matches[j];
+                if (match.idx >= 0) {
+                    sourcePoints.push_back(transformedPoints[j]);
+                    targetPoints.push_back(m_target.points[match.idx]);
+                }
+            }
+
+            estimatedPose = solvePointToPlane(sourcePoints, targetPoints, m_target.normals) * estimatedPose;
+        }
+
+        return estimatedPose;
+
+    }
 
 private:
+    Matrix4f solvePointToPlane(const std::vector<Vector3f>& sourcePoints, const std::vector<Vector3f>& targetPoints, const std::vector<Vector3f>& targetNormals)
+    {
+        const unsigned nPoints = sourcePoints.size();
+
+        // Build the system
+        MatrixXf A = MatrixXf::Zero(4 * nPoints, 6);
+        VectorXf b = VectorXf::Zero(4 * nPoints);
+
+        for (unsigned i = 0; i < nPoints; i++) {
+            const auto& s = sourcePoints[i];
+            const auto& d = targetPoints[i];
+            const auto& n = targetNormals[i];
+
+            // Add the point-to-plane constraints to the system
+            b[i] = n.dot(d) - n.dot(s);
+            A.block<1,3>(i,0) << n[2]*s[1] - n[1]*s[2],
+                                 n[0]*s[2] - n[2]*s[0],
+                                 n[1]*s[0] - n[0]*s[1];
+            A.block<1,3>(i,3) = n;
+            // Add the point-to-point constraints to the system
+            // for x-coords
+            b[nPoints+i] = d[0] - s[0];
+            A.block<1,6>(nPoints+i,0) << 0, s[2], -s[1], 1, 0, 0;
+
+            // for y-coords
+            b[2*nPoints+i] = d[1] - s[1];
+            A.block<1,6>(2*nPoints+i,0) << -s[2], 0, s[0], 0, 1, 0;
+
+            // for z-coords
+            b[3*nPoints+i] = d[2] - s[2];
+            A.block<1,6>(3*nPoints+i,0) << s[1], -s[0], 0, 0, 0, 1;
+        }
+
+        // Solve the system
+        VectorXf x(6);
+        JacobiSVD<MatrixXf> svd(A, ComputeThinU | ComputeThinV);
+
+        x = svd.solve(b);
+
+        float alpha = x(0), beta = x(1), gamma = x(2);
+
+        // Build the pose matrix
+        Matrix3f rotation = AngleAxisf(alpha, Vector3f::UnitX()).toRotationMatrix() *
+                            AngleAxisf(beta, Vector3f::UnitY()).toRotationMatrix() *
+                            AngleAxisf(gamma, Vector3f::UnitZ()).toRotationMatrix();
+
+        Vector3f translation = x.tail(3);
+
+        // Build the pose matrix using the rotation and translation matrices
+        Matrix4f estimatedPose = Matrix4f::Identity();
+        estimatedPose.block<3,3>(0,0) = rotation;
+        estimatedPose.block<3,1>(0,3) = translation;
+
+        return estimatedPose;
+    }
     std::unique_ptr<NearestNeighborSearch> m_nearestNeighborSearch;
 };
 
@@ -311,6 +401,20 @@ public:
             return true;
         }
         m_SurfaceMeasurer->registerInput(m_InputHandle->getDepth());
+        m_SurfaceMeasurer->process();
+
+        PointCloud nextFrame = m_SurfaceMeasurer->getPointCloud();
+
+        std::vector<bool> pointsAndNormalsValid;
+        pointsAndNormalsValid.reserve( nextFrame.pointsValid.size() );
+
+        std::transform(nextFrame.pointsValid.begin(), nextFrame.pointsValid.end(), nextFrame.normalsValid.begin(),
+            std::back_inserter(pointsAndNormalsValid), std::logical_and<>());
+
+
+        m_PoseEstimator = new NearestNeighborPoseEstimator();
+        m_PoseEstimator->setSource(PoseEstimator::pruneVector(nextFrame.points, pointsAndNormalsValid),
+                                   PoseEstimator::pruneVector(nextFrame.normals, pointsAndNormalsValid));
 
 
         return false;
